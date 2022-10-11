b6962066b52f823d53a592fdaad885af850629d4
sagar <skatahre@gmail.com>
2022-10-11 22:45:51 +0530

    Calculate employee wage

---
+++ EmployeeWage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY8
+{
+    public class EmployeeWage
+    {
+
+        public void Calculate()
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_PerHr = 20;
+
+            int EmpHrs ;
+            int EmpWages ;
+
+
+            Random random = new Random();
+            int EmpCheck = random.Next(2);
+
+            if (EmpCheck == Is_Full_Time)
+            {
+                EmpHrs = 8;
+            }
+            else
+            {
+                EmpHrs = 0;
+            }
+            EmpWages = EmpHrs * Emp_Rate_PerHr;
+            Console.WriteLine("Employee Daily Wages are : " + EmpWages);
+        }
+
+
+    }
+}
