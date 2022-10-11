8ad06aaa306ffae3b4b20656900b8718a4c3e7fc
sagar <skatahre@gmail.com>
2022-10-11 19:02:21 +0530

    Check employee Attendance

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY8
+{
+    public class EmployeeAttendance
+    {
+        public void Check()
+        {
+            
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 2);
+
+            if (EmpCheck == 1)
+            {
+                Console.WriteLine("Empolyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
