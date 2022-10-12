429ffe249a04ff5f583dea8a0a6bf045a5963ce3
sagar <skatahre@gmail.com>
2022-10-12 18:35:45 +0530

    Calculating Wages for a Month using class

--- Program.cs
+++ Program.cs
@@ -12,8 +12,9 @@ namespace DAY8
         {
             Console.WriteLine(" \t Practice problem day 8");
             Console.WriteLine("\n --------------------------\n");
-           MonthlyWage2 monthlyWage2 = new MonthlyWage2();
-            monthlyWage2.Calculate2();
+           
+
+            RefractorClass.ComputeEmpWage();
         }
     }
 }
