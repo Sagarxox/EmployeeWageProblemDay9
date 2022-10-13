3eaba69fed8a0d77c72d57d049bf4766117670ef
sagar <skatahre@gmail.com>
2022-10-14 01:15:33 +0530

    multiple company

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" \t Practice problem day 8");
+            Console.WriteLine("\n --------------------------\n");
+
+
+           MultipleCompany company1 = new MultipleCompany();
+           company1.wages("Dmart", 20, 12, 30);
+            MultipleCompany company2 = new MultipleCompany();
+            company2.wages("jio", 19, 10, 20);
+
+        }
+    }
+}
